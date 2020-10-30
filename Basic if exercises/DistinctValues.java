a171f8c6d44cebdb81e9b4e920e373c1ac528db2
Jose Luis <sareeply@hotmail.com>
2020-10-30 15:01:45 +0100

    Listo para enviar

---
+++ Basic if exercises/DistinctValues.java
@@ -0,0 +1,16 @@
+import java.lang.System.*;
+
+public class DistinctValues {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		if (a == b && a == c) {
+			System.out.println("1"); }
+		else if (a != b && a != c && b != c ) {
+			System.out.println("3"); }
+		else {
+			System.out.println("2"); }
+	}
+}
